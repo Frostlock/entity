88957efc4e4f86470d7bb59ca13db06899549a8b
Frostlock <pieter.boogaerts@gmail.com>
2017-09-21 20:31:15 +0200

    Google add task to tasklist

--- actions/GoogleActionListTasks.py
+++ actions/GoogleActionListTasks.py
@@ -6,6 +6,7 @@ import os
 from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
+from oauth2client.client import HttpAccessTokenRefreshError
 from oauth2client.file import Storage
 
 try:
@@ -16,50 +17,65 @@ except ImportError:
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/tasks-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/tasks.readonly'
-CLIENT_SECRET_FILE = 'GoogleClientSecret.json'
-APPLICATION_NAME = 'Google Tasks API Python Quickstart'
+SCOPES = "https://www.googleapis.com/auth/tasks"
+CLIENT_SECRET_FILE = "GoogleClientSecret.json"
+APPLICATION_NAME = "Google Tasks API Python Quickstart"
+
+CREDENTIAL_DIR = ".credentials"
+CREDENTIAL_FILE = "tasks-python-quickstart.json"
 
 ACTION_GROUP = "Google services"
 ACTION_GROUP_DESCRIPTION = "Access to several Google services."
 
 
+def clear_credentials():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, CREDENTIAL_DIR)
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir, CREDENTIAL_FILE)
+    os.remove(credential_path)
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, CREDENTIAL_DIR)
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir, CREDENTIAL_FILE)
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+
 class GoogleActionListTasks(Action):
+    """
+    Action to enable Entity to list current tasks on Google tasklist
+    Based on example : https://developers.google.com/google-apps/tasks/quickstart/python
+    """
 
     _action_name = "List Google tasks"
     _action_description = "List current google tasks."
-    _command_words = ["what are my to do's", "what are my tasks", "list my tasks", "to do list", "tasks"]
+    _command_words = ["what are my to do's", "what are my tasks", "list my tasks", "to do list", "tasks", "list tasks"]
 
     def __init__(self, entity):
         super(GoogleActionListTasks, self).__init__(entity)
-
-    def get_credentials(self):
-        """Gets valid user credentials from storage.
-
-        If nothing has been stored, or if the stored credentials are invalid,
-        the OAuth2 flow is completed to obtain the new credentials.
-
-        Returns:
-            Credentials, the obtained credential.
-        """
-        home_dir = os.path.expanduser('~')
-        credential_dir = os.path.join(home_dir, '.credentials')
-        if not os.path.exists(credential_dir):
-            os.makedirs(credential_dir)
-        credential_path = os.path.join(credential_dir,
-                                       'tasks-python-quickstart.json')
-
-        store = Storage(credential_path)
-        credentials = store.get()
-        if not credentials or credentials.invalid:
-            flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-            flow.user_agent = APPLICATION_NAME
-            if flags:
-                credentials = tools.run_flow(flow, store, flags)
-            else: # Needed only for compatibility with Python 2.6
-                credentials = tools.run(flow, store)
-            self.log('Storing credentials to ' + credential_path)
-        return credentials
 
     def respond(self, command):
         """
@@ -69,11 +85,21 @@ class GoogleActionListTasks(Action):
         API documentation: https://developers.google.com/resources/api-libraries/documentation/tasks/v1/python/latest/index.html
         """
         TASKLIST_ID = "MTM5NzE3NjQyMTYwMjY5Nzk0MjQ6MDow"
-        credentials = self.get_credentials()
+
+        credentials = get_credentials()
         http = credentials.authorize(httplib2.Http())
         service = discovery.build('tasks', 'v1', http=http)
+        try:
+            results = service.tasks().list(tasklist=TASKLIST_ID).execute()
+        except HttpAccessTokenRefreshError as e:
+            print(e)
+            print("Clearing credentials and tyring again.")
+            clear_credentials()
+            credentials = get_credentials()
+            http = credentials.authorize(httplib2.Http())
+            service = discovery.build('tasks', 'v1', http=http)
+            results = service.tasks().list(tasklist=TASKLIST_ID).execute()
 
-        results = service.tasks().list(tasklist=TASKLIST_ID).execute()
         items = results.get('items', [])
         if not items:
             return "Your task list is empty."
@@ -87,6 +113,67 @@ class GoogleActionListTasks(Action):
             return answer[:-2]
 
 
+class GoogleActionAddTask(Action):
+    """
+    Enables Entity to add a new task to the Google tasklist
+    """
+
+    _action_name = "Add Google task"
+    _action_description = "Add a new task to the Google tasklist."
+    _command_words = ["add task", "new task", "create task"]
+
+    def __init__(self, entity):
+        super(GoogleActionAddTask, self).__init__(entity)
+
+    def match(self, command):
+        """
+        Return whether or not this action matches with the provide command
+        :return: True or False
+        """
+        for command_start in self.command_words:
+            if command.startswith(command_start):
+                return True
+        return False
+
+    def respond(self, command):
+        """
+        Adds a task to the tasklist.
+        Tasklist ID is currently hardcoded.
+
+        Documentation: https://developers.google.com/google-apps/tasks/v1/reference/tasks/insert
+        """
+        TASKLIST_ID = "MTM5NzE3NjQyMTYwMjY5Nzk0MjQ6MDow"
+
+        task_title = ""
+        for command_start in self.command_words:
+            if command.startswith(command_start):
+                task_title = command.lstrip(command_start).lstrip()  # Also remove likely space
+                break
+
+        if task_title == "":
+            return "Please repeat command and add a name for the new task."
+
+        task = {
+            'title': task_title,
+            'notes': "Task added by entity"
+        }
+
+        try:
+            credentials = get_credentials()
+            http = credentials.authorize(httplib2.Http())
+            service = discovery.build('tasks', 'v1', http=http)
+            result = service.tasks().insert(tasklist=TASKLIST_ID, body=task).execute()
+            return "Task added"
+        except HttpAccessTokenRefreshError as e:
+            print(e)
+            print("Clearing credentials and tyring again.")
+            clear_credentials()
+            credentials = get_credentials()
+            http = credentials.authorize(httplib2.Http())
+            service = discovery.build('tasks', 'v1', http=http)
+            result = service.tasks().insert(tasklist=TASKLIST_ID, body=task).execute()
+            return "Task added"
+
 if __name__ == '__main__':
-    GA = GoogleActionListTasks()
+    GA = GoogleActionListTasks(None)
     print(GA.respond("brol"))
