cf6c139b8e1c23548e0a023bf411501eab7c941f
Frostlock <pieter.boogaerts@gmail.com>
2017-10-27 11:04:01 +0200

    added ping sweep

--- components/OsCommand-GetIpForMac.sh
+++ components/OsCommand-GetIpForMac.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+# Query arp cache for IP
+# Note that a ping sweep might be helpful to populate the arp cache
 arp -n | grep $1 | awk '{print $1}'
--- components/OsCommand.py
+++ components/OsCommand.py
@@ -1,6 +1,15 @@
-from subprocess import run, PIPE
+from subprocess import Popen, run, PIPE, TimeoutExpired
 
 SCRIPTS_PATH = "./components/"
+
+def ping_sweep():
+    """
+    Triggers a ping sweep which will populate the arp table.
+    The call will return immediately and the ping sweep will run in the background.
+    :return: Popen object to interact with the ping sweep process
+    """
+    return Popen([SCRIPTS_PATH + "OsCommand-PingSweep.sh"])
+
 def get_ip_for_mac(mac):
     """
     Calls a shell script to retrieve the IP for the given Mac address.
@@ -15,3 +24,14 @@ def get_ip_for_mac(mac):
         return lines[0]
     else:
         return None
+
+if __name__ == '__main__':
+    SCRIPTS_PATH = "./"
+    p = ping_sweep()
+    while p.poll() is None:
+        print("Waiting on ping sweep")
+        try:
+            p.wait(timeout=5)
+        except TimeoutExpired:
+            pass
+    #print(get_ip_for_mac("00:17:88:71:33:87"))
